45dd965e34df69bc24e06bdc81173fcc3f8568f5
Maxim Grebenev <maks.grebenev.958@mail.ru>
2026-09-12 17:56:32 +0300

    Initial commit: project files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, my name is Maxim. I am from IU5-11/4!" << std::endl;
+    std::cout << "Updated message!" << std::endl;
     return 0;
 }
